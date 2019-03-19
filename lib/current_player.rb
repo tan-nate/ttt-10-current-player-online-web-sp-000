57583415b10f3280c4200e55103a816c88509795
Nate Tan <nktan93@gmail.com>
2019-03-19 21:01:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,12 @@
-def turn_count
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+end
+
+def current_player(board)
   
 end
